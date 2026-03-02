3e17d62ba51ef5e8d74614bf14a1e073b5fedc85
vk2765 <vk2765@srmist.edu.in>
2026-03-02 10:12:15 +0530

    Vaishnavi added uc1

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,23 @@
+public class UseCase1PalindromeCheckerApp {
+
+    /**
+     * Application entry point.
+     *
+     * This is the first method executed by the JVM
+     * when the program starts.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("   Welcome to Palindrome Checker App");
+
+        // Application Details
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version          : 1.0");
+
+        System.out.println("Application started successfully.");
+        System.out.println("Ready for next use case.");
+    }
+}
